31c49ade2d0c05318e101062c6759325f6788c68
Vincent Plikat <vincent.plikat@student.uni-tuebingen.de>
2021-02-03 16:46:03 +0100

    Now we do skip the dummy scans !!! This has to be kept in mind when doing preprocessing and first level analysis. Also now we use spm_select rather than dir, since dir is not case sensitive

--- DICOMconversionSPM12_BIDS.m
+++ DICOMconversionSPM12_BIDS.m
@@ -27,15 +27,34 @@ end
 prefix = input (['Please specify the prefix of your participant data.\n' ...
     '(like p for participant or s for subject. It has to be unique so that only subject folders are selected):\n'],'s');
 
+%-------------------------------------------------------------------------%
+% DEFINE file extensions you want sort
+extensions = {'**.IMA','**.ima'}; % extension you care about
+% IMPORTANT: It seems like dir() (at least on MacOS) is not case sensitive,
+% but spm_select('FPList',...) is
+%-------------------------------------------------------------------------%
+
+nDummies = input(['Please specify the number of dummy images.\n'...
+    '!!!IMPORTANT!!!\n'...
+    'Be super duper sure to give the right number of images, because those will be skipped in the conversion process!\n\n']);
+
+% Safetynet, that ensures the input for dummy images is actually numeric
+correctInput = isnumeric(nDummies);
+while ~correctInput
+    nDummies = input(['It seems your input for the number of dummy images is not numeric.\n'...
+        'Please specify the number of dummy images again (do not forget to be super duper sure).\n']);
+    correctInput = isnumeric(nDummies);
+end
+
 dic_struct_dir  = 'anat/';
 
 raw_dir         = fullfile(root_dir, 'raw_data');
 
 %% Decide what to do
 %..............................WHAT TO DO.................................%
-do.overwrite        = 0;
-do.dicom_conversion = 1;
-do.struct_conversion= 1;
+do.overwrite            = 0;
+do.func_conversion      = 1;
+do.struct_conversion    = 1;
 
 % TODO: create a log file to save warning and error messages
 
@@ -54,102 +73,99 @@ spm_mkdir(raw_dir, subNames, 'func');
 %% start to perform the conversion
 for ss = 1:length(subNames) % For all subjects do each ...
     
+    nifti_dir = fullfile(raw_dir,subNames{ss});
+    func_nifti_dir = fullfile(nifti_dir,'func');
     
-        nifti_dir = fullfile(raw_dir,subNames{ss});
-        func_nifti_dir = fullfile(nifti_dir,'func');
-
-        %% Conversion from DICOM to NIfTI
-        if do.dicom_conversion
-            dicom_dir       = fullfile(source_dir,subNames{ss});
-            func_dicom_dir  = fullfile(dicom_dir,'func');
-            folderContent   = dir(fullfile(func_dicom_dir,'run*')); 
-            %% create a BIDS conform file structure for every subject
-            % source data: DICOMS
-            spm_mkdir (raw_dir, subNames{ss}, 'func', {folderContent(:).name});
-            % ......DICOM to NIFTI Conversion...... %
-            % Get folder Content
-
-            if isempty(folderContent) % no dicoms in folder
-                warning('FOLDER CONTENT IS EMPTY - PROBABLY WRONG PATH: process stopped. Press Enter to continue.')
-                disp(['SUBJECTS PATH:  ' subj(ss).fol_dicom])
-                pause;
-            else
-                fprintf('FOLDER CONTENT FOUND \n')
-                fprintf('========STARTING CONVERTION FROM NIFTI TO DICOM========\n\n');
-
-                for i = 1:length(folderContent)            % loop through all folders found  
-                    try                      
-                        curr_dir = fullfile(func_dicom_dir, folderContent(i).name);
-                        dest_dir = fullfile(func_nifti_dir, folderContent(i).name);
-
-                        % select files (either "ima" or "IMA") % try to
-                        % regularize for capital or not.
-                        dirfiles = spm_select('FPList', curr_dir, '.*ima');
-                        if isempty(dirfiles)
-                            dirfiles = spm_select('FPList', curr_dir, '.*IMA');
-                            if isempty(dirfiles)
-                                warning('NO *ima nor *IMA  FILES SELECTED - PROBABLY WRONG PATH/FILENAME: process stopped. Press Enter to continue.')
-                                disp(['CURRENT PATH:  ' curr_dir]);
-                                pause;
-                            end
-                        end
-
-                        % specify spm options
-                        matlabbatch{1}.spm.util.import.dicom.data               = cellstr(dirfiles);
-                        matlabbatch{1}.spm.util.import.dicom.root               = 'flat';
-                        matlabbatch{1}.spm.util.import.dicom.outdir             = cellstr(dest_dir);
-                        matlabbatch{1}.spm.util.import.dicom.protfilter         = '.*';
-                        matlabbatch{1}.spm.util.import.dicom.convopts.format    = 'nii';
-                        matlabbatch{1}.spm.util.import.dicom.convopts.meta      = 0;
-                        matlabbatch{1}.spm.util.import.dicom.convopts.icedims   = 0;
-
-                        fprintf('=> importing dicoms from %s\n', curr_dir);
-                        fprintf('                      to %s\n', dest_dir);
-
-                        spm_jobman('run', matlabbatch); 
-
-                    catch ME
-                        warning('Dicom in %s could not be converted.\n',folderContent(i).name)
-                        sprintf('Error in function %s() at line %d.\n\nError Message:\n%s', ...
-                            ME.stack(1).name, ME.stack(1).line, ME.message);
+    %% Conversion from functional DICOM to NIfTI
+    if do.func_conversion
+        % where to find functional data
+        dicom_dir       = fullfile(source_dir,subNames{ss});
+        func_dicom_dir  = fullfile(dicom_dir,'func');
+        folderContent   = dir(fullfile(func_dicom_dir,'run*'));
+        %% create a BIDS conform file structure for every subject
+        % source data: DICOMS
+        spm_mkdir (raw_dir, subNames{ss}, 'func', {folderContent(:).name});
+        % ......DICOM to NIFTI Conversion...... %
+        % Get folder Content
+        
+        if isempty(folderContent) % no dicoms in folder
+            warning('FOLDER CONTENT IS EMPTY - PROBABLY WRONG PATH: process stopped. Press Enter to continue.')
+            disp(['SUBJECTS PATH:  ' subj(ss).fol_dicom])
+            pause;
+        else
+            fprintf('FOLDER CONTENT FOUND \n')
+            fprintf('========STARTING CONVERTION FROM NIFTI TO DICOM========\n\n');
+            
+            for i = 1:length(folderContent)            % loop through all folders found
+                try
+                    curr_dir = fullfile(func_dicom_dir, folderContent(i).name);
+                    dest_dir = fullfile(func_nifti_dir, folderContent(i).name);
+                    
+                    dirfiles = [];
+                    for ext = 1:length(extensions)
+                        dirfiles = [dirfiles; spm_select('FPList', curr_dir, extensions{ext})];
                     end
-                    % write JSON file
-                    % give the function the directory where to store the
-                    % json file and the last dicom directory to read out 
-                    % necessary information
-%                     BIDS_bold_json (curr_dir,dirfiles(1))
-                end            
-            end
-        end
-
-        %% Conversion from DICOM to NIfTI of STRUCTURAL image
-        % (just to have it in another folder and change name)
-        if do.struct_conversion
-            curr_dir = fullfile(dicom_dir,'anat');
-            dest_dir = fullfile(nifti_dir, dic_struct_dir);
-
-            % select files (either "ima" or "IMA")
-            dirfiles = spm_select('FPList', curr_dir, '.*ima');
-            if isempty(dirfiles)
-                dirfiles = spm_select('FPList', curr_dir, '.*IMA');
-                if isempty(dirfiles)
-                    warning('NO *ima nor *IMA  FILES SELECTED - PROBABLY WRONG PATH/FILENAME: process stopped. Press Enter to continue.')
-                    disp(['CURRENT PATH:  ' curr_dir]);
-                    pause;
+                    if isempty(dirfiles)
+                        warning('NO FILES WITH YOUR SPECIFIED FILEEXTENTION SELECTED - PROBABLY WRONG PATH/FILENAME: process stopped. Press Enter to continue.')
+                        disp(['CURRENT PATH:  ' curr_dir]);
+                        pause;
+                    end
+                    
+                    % specify spm options
+                    matlabbatch{1}.spm.util.import.dicom.data               = cellstr(dirfiles(nDummies+1:end,:));
+                    matlabbatch{1}.spm.util.import.dicom.root               = 'flat';
+                    matlabbatch{1}.spm.util.import.dicom.outdir             = cellstr(dest_dir);
+                    matlabbatch{1}.spm.util.import.dicom.protfilter         = '.*';
+                    matlabbatch{1}.spm.util.import.dicom.convopts.format    = 'nii';
+                    matlabbatch{1}.spm.util.import.dicom.convopts.meta      = 0;
+                    matlabbatch{1}.spm.util.import.dicom.convopts.icedims   = 0;
+                    
+                    fprintf('=> importing dicoms from %s\n', curr_dir);
+                    fprintf('                      to %s\n', dest_dir);
+                    
+                    spm_jobman('run', matlabbatch);
+                    
+                catch ME
+                    warning('Dicom in %s could not be converted.\n',folderContent(i).name)
+                    sprintf('Error in function %s() at line %d.\n\nError Message:\n%s', ...
+                        ME.stack(1).name, ME.stack(1).line, ME.message);
                 end
+                % write JSON file
+                % give the function the directory where to store the
+                % json file and the last dicom directory to read out
+                % necessary information
+                %                     BIDS_bold_json (curr_dir,dirfiles(1))
             end
-
-            fprintf('=> importing structural to %s\n', dest_dir);
-            % specify spm options
-            matlabbatch{1}.spm.util.import.dicom.data               = cellstr(dirfiles);
-            matlabbatch{1}.spm.util.import.dicom.root               = 'flat';
-            matlabbatch{1}.spm.util.import.dicom.outdir             = cellstr(dest_dir);
-            matlabbatch{1}.spm.util.import.dicom.protfilter         = '.*';
-            matlabbatch{1}.spm.util.import.dicom.convopts.format    = 'nii';
-            matlabbatch{1}.spm.util.import.dicom.convopts.meta      = 1;
-            matlabbatch{1}.spm.util.import.dicom.convopts.icedims   = 0;
-
-            spm_jobman('run', matlabbatch);
         end
+    end
     
+    %% Conversion from DICOM to NIfTI of STRUCTURAL image
+    % (just to have it in another folder and change name)
+    if do.struct_conversion
+        curr_dir = fullfile(dicom_dir,'anat');
+        dest_dir = fullfile(nifti_dir, dic_struct_dir);
+        
+        % select files (either "ima" or "IMA")
+        dirfiles = [];
+        for ext = 1:length(extensions)
+            dirfiles = [dirfiles; spm_select('FPList', curr_dir, extensions{ext})];
+        end
+        if isempty(dirfiles)
+            warning('NO FILES WITH YOUR SPECIFIED FILEEXTENTION SELECTED - PROBABLY WRONG PATH/FILENAME: process stopped. Press Enter to continue.')
+            disp(['CURRENT PATH:  ' curr_dir]);
+            pause;
+        end
+        
+        fprintf('=> importing structural to %s\n', dest_dir);
+        % specify spm options
+        matlabbatch{1}.spm.util.import.dicom.data               = cellstr(dirfiles);
+        matlabbatch{1}.spm.util.import.dicom.root               = 'flat';
+        matlabbatch{1}.spm.util.import.dicom.outdir             = cellstr(dest_dir);
+        matlabbatch{1}.spm.util.import.dicom.protfilter         = '.*';
+        matlabbatch{1}.spm.util.import.dicom.convopts.format    = 'nii';
+        matlabbatch{1}.spm.util.import.dicom.convopts.meta      = 1;
+        matlabbatch{1}.spm.util.import.dicom.convopts.icedims   = 0;
+        
+        spm_jobman('run', matlabbatch);
+    end
 end
