9ded02106b57d1c10ba6b62ad449a048f00ba137
PabloRG <pablo.grassi@cin.uni-tuebingen.de>
2025-06-06 22:14:57 +0200

    update

--- BIDS_irods/createSequenceMat.m
+++ BIDS_irods/createSequenceMat.m
@@ -20,12 +20,30 @@ newSequence={"fmap", "gre_field_mapping", {[]}, "bold"}; %#ok<*CLARRSTR>
 logs.ses(1).tbl = [logs.ses(1).tbl; newSequence];
 
 %% Second session sequences
+%% First session sequences
+logs.ses(2).tbl = cell2table(cell(0,4),'VariableNames', Headers);
 
+% Anatomical: Add the new sequence to table
+newSequence={"anat", "fastADNI_192sl_TMS_3_56", {192}, "T1w"}; %#ok<*STRSCALR
+logs.ses(2).tbl = [logs.ses(2).tbl; newSequence];
+
+% Find my beans: Add the new sequence to table
+newSequence={"findtms", "t1_fl2d_sag_find_my_beans_AP", {[]}, "T1w"}; %#ok<*STRSCALR
+logs.ses(2).tbl = [logs.ses(2).tbl; newSequence];
+
+
+% Functional: Add the new sequence to table
+newSequence={"func", "ep2d_3mm_TE30_64channel_MB2_GRAPPA2_500ms_TMS_session", {[331]}, "bold"}; 
+logs.ses(2).tbl = [logs.ses(2).tbl; newSequence];
+
+% Fieldmaps: Add the new sequence to table
+newSequence={"fmap", "gre_field_mapping", {[]}, "bold"}; %#ok<*CLARRSTR> 
+logs.ses(2).tbl = [logs.ses(2).tbl; newSequence];
 
 
 %% Display the table
 disp(logs.ses(1).tbl);
-
+disp(logs.ses(2).tbl);
 save('sequenceInfo.mat',"logs");
 
 % Before:
